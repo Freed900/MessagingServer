7d2d44bbc8efd71432aa1038b40d64da556052a1
Freed900 <90863224+Freed900@users.noreply.github.com>
2021-12-16 19:29:28 +0200

    Add files via upload

--- node js/server1.js
+++ node js/server1.js
@@ -1,31 +1,62 @@
-const express = require("express");
-const app = express();
-
 const hostname = '127.0.0.1';
 const port = 3000;
+const { Console } = require("console");
+
+var http = require("http");
+var querystring = require('querystring');
 
 var messages = [];
 var MessagesCapacity=5;
 
-app.get("/getMessages", function(request, response){
-  response.send(messages);
-});
+http.createServer(function (request, response) {
 
-app.get("/sendMessage", function(request, response){
-  var m =request.query.message;
   
-  if(m.length<=0) {
-    response.send("Сообщение слишком короткое!");
+
+  if (request.method == 'GET') {
+    var methodName = request.headers["method-name"];
+    if (methodName == "SendMessage") {
+      var m = request.headers["message-text"];
+      SendMessage(m, response);
+    }
+    else if (methodName == "GetMessages") {
+      
+      GetMessages(response);
+
+    }
+  }
+  else if (request.method == 'POST') {
+    var index = request.url.indexOf('?')+1;
+    var s = request.url.substr(index, request.url.length - index);
+    var data = querystring.parse(s);
+
+    if (data['method-name'] == "SendMessage") {
+      SendMessage(data["message-text"], response);
+    }
+    else if (data['method-name'] == "GetMessages") {
+      GetMessages(response);
+    }
+    
+  }
+  response.end();
+}).listen(port);
+
+function GetMessages(response) {
+  try{
+    response.write(messages.toString());
+  }
+  catch (ex) {
+    console.log(ex)
+  }
+};
+
+function SendMessage( m,response) {
+  if (m.length <= 0) {
+    response.write("Сообщение слишком короткое!");
     return;
   }
 
   messages.unshift(m);
-  while(messages.length>MessagesCapacity)
+  while (messages.length > MessagesCapacity)
     messages.pop();
-
-  response.send("Сообщение доставлено!");
-});
-
-app.listen(port,hostname,()=>{
-  console.log("Server started on "+port);
-});
+  response.write("Сообщение доставлено!");
+}
