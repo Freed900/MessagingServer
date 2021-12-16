dd73653d4d4037d84c967066c6ef0e1ea5d3fdbf
Freed900 <90863224+Freed900@users.noreply.github.com>
2021-12-16 19:30:08 +0200

    Add files via upload

---
+++ WebServer/Program.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using WebServer;
+
+BuildWebHost(args).Run();
+
+static IWebHost BuildWebHost(string[] args) =>
+        WebHost.CreateDefaultBuilder(args)
+        .UseStartup<Startup>()
+        .Build();
---
+++ WebServer/Startup.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebServer
+{
+    internal class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.Run(async context =>
+            {
+                if (context.Request.Query.ContainsKey("GetMessages"))
+                {
+                    StringBuilder msgs = new StringBuilder();
+                    for(int i=messages.Count-1,t=5;i>=0 && t > 0; i--, t--)
+                    {
+                        msgs.Append(messages[i]).Append("<br>");
+                    }
+                    await context.Response.WriteAsync(msgs.ToString());
+                }
+                else
+                {
+                    if (context.Request.Query.ContainsKey("NewMessage"))
+                    {
+                        messages.Add(context.Request.Query["NewMessage"]);
+                    }
+                }
+                
+            });
+        }
+
+        List<string> messages = new List<string>();
+
+    }
+}
